21be3da53ef5142d90183717056a3ded584b0360
Hequn Zhang <heqzha@gmail.com>
2017-07-17 11:01:11 +0800

    Add mutex for msg queue

--- core/message.go
+++ core/message.go
@@ -7,16 +7,18 @@ import (
 )
 
 type MessageQueue struct {
-	q    map[string]*container.Queue
-	sync *sync.Mutex
+	q     map[string]*container.Queue
+	mutex *sync.RWMutex
 }
 
 func (m *MessageQueue) Init() {
 	m.q = make(map[string]*container.Queue)
-	m.sync = &sync.Mutex{}
+	m.mutex = &sync.RWMutex{}
 }
 
 func (m *MessageQueue) Push(chn string, msg interface{}) {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	_, ok := m.q[chn]
 	if !ok {
 		m.q[chn] = new(container.Queue)
@@ -25,6 +27,8 @@ func (m *MessageQueue) Push(chn string, msg interface{}) {
 }
 
 func (m *MessageQueue) Pop(chn string) interface{} {
+	m.mutex.Lock()
+	defer m.mutex.Unlock()
 	c, ok := m.q[chn]
 	if ok && c != nil {
 		return c.Pop()
@@ -33,5 +37,7 @@ func (m *MessageQueue) Pop(chn string) interface{} {
 }
 
 func (m *MessageQueue) Len(chn string) int {
+	m.mutex.RLock()
+	defer m.mutex.RUnlock()
 	return m.q[chn].Len()
 }
