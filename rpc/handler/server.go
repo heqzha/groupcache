814904b85302e08b3fe82481c7c02473b3099938
Hequn Zhang <heqzha@gmail.com>
2017-07-18 17:07:14 +0800

    Update rpc.handler

--- rpc/handler/server.go
+++ rpc/handler/server.go
@@ -1,20 +1,39 @@
 package handler
 
+import (
+	"fmt"
+
+	"github.com/heqzha/dcache/core"
+	"github.com/heqzha/dcache/utils"
+	"github.com/heqzha/goutils/logger"
+)
+
+var (
+	sgm  = utils.GetSGMInst()
+	msgQ = utils.GetMsgQInst()
+)
+
 func Register(group, addr string) error {
-	sgm := GetSGMInst()
 	sgm.Register(group, addr)
-	//TODO activate sync process
+	msgQ.Push("srvgroup", map[string]interface{}{
+		"type": "sync",
+	})
 	return nil
 }
 
 func Unregister(group, addr string) error {
-	sgm := GetSGMInst()
 	sgm.Unregister(group, addr)
-	//TODO activate sync process
+	msgQ.Push("srvgroup", map[string]interface{}{
+		"type": "sync",
+	})
 	return nil
 }
 
-func SyncSrvGroups(srvgroups []byte) error {
-	//TODO handle sync request
-	return nil
+func SyncSrvGroups(srvgroups []byte) (core.Condition, *core.SGM, error) {
+	tmpSGM := core.SGM{}
+	tmpSGM.Load(srvgroups)
+	logger.Info("SGM.Merge", fmt.Sprintf("Before Merge: %s", sgm.CompareReadable(tmpSGM)))
+	cond := sgm.Merge(tmpSGM)
+	logger.Info("SGM.Merge", fmt.Sprintf("After Merge: %s", sgm.CompareReadable(tmpSGM)))
+	return cond, sgm, nil
 }
